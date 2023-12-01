31e4897f538345d6ac533a77de8b61b4210a5a5b
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-01 12:09:20 +0300

    fixed errors in regards to the signup api route

--- api/index.js
+++ api/index.js
@@ -4,10 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cors from 'cors'
 
 const app = express ();
 
 app.use(express.json());
+
+app.use(cors());
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to the Database')
