8a1398d405849b0fbbab77fe90048b58a82240a6
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-12-01 11:12:29 +0300

    created test api routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
+import userRouter from './routes/user.route.js'
 
 const app = express ();
 
@@ -15,3 +16,4 @@ app.listen(3000, () => {
     console.log('Server is Running')
 })
 
+app.use('/api/user',userRouter);
